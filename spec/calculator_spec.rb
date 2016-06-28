0553df6a5dfe106c1519289912d5ee72d4c06d78
Raul Vega <Raulvv@Rauls-MacBook-Air.local>
2016-06-28 11:22:00 +0200

    battery test for iteration 1 and 2

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -9,10 +9,10 @@ RSpec.describe('Online Calculator self-corrected exercise') do
     Sinatra::Application
   end
 
+  let(:calculator){ Calculator.new }
+
   context 'Iteration 1' do
     describe '#add' do
-      let(:calculator){ Calculator.new }
-
       context 'when not valid' do
         it 'receive two parameters' do
           expect(calculator).to respond_to(:add).with(2).argument
@@ -36,6 +36,79 @@ RSpec.describe('Online Calculator self-corrected exercise') do
   end
 
   context 'Iteration 2' do
+    describe '#substract' do
+      context 'when not valid' do
+        it 'receive two parameters' do
+          expect(calculator).to respond_to(:substract).with(2).argument
+        end
+
+        it 'omits the letters as parameters' do
+          expect(calculator.substract(1, "a")).to eq(1)
+        end
+      end
+
+      context 'when valid' do
+        it 'substracts the parameters' do
+          expect(calculator.substract(1,1)).to eq(0)
+        end
+
+        it 'substracts the parameters as integers' do
+          expect(calculator.substract("1", "1")).to eq(0)
+        end
+      end
+    end
+
+    describe '#multiply' do
+      context 'when not valid' do
+        it 'receive two parameters' do
+          expect(calculator).to respond_to(:multiply).with(2).argument
+        end
+
+        it 'omits the letters as parameters' do
+          expect(calculator.multiply(1, "a")).to eq(0)
+        end
+      end
+
+      context 'when valid' do
+        it 'multiplies the parameters' do
+          expect(calculator.multiply(1,1)).to eq(1)
+        end
+
+        it 'multiplies the parameters as integers' do
+          expect(calculator.multiply("1", "1")).to eq(1)
+        end
+      end
+    end
+
+    describe '#divide' do
+      context 'when not valid' do
+        it 'receive two parameters' do
+          expect(calculator).to respond_to(:divide).with(2).argument
+        end
+
+        it 'omits the letters as parameters' do
+          expect(calculator.divide("a", 1)).to eq(0)
+        end
+      end
+
+      context 'when valid' do
+        it 'divides the parameters' do
+          expect(calculator.divide(4, 2)).to eq(2)
+        end
+
+        it 'divides the parameters as integers' do
+          expect(calculator.divide("4", "2")).to eq(2)
+        end
+
+        it 'returns "INFINITE" where the second parameter is 0' do
+          expect(calculator.divide(1, 0)).to eq('INFINITE')
+        end
+
+        it 'returns "INFINITE" where the second parameter is a letter' do
+          expect(calculator.divide(1, "a")).to eq('INFINITE')
+        end
+      end
+    end
   end
 
   context 'Iteration 3' do
